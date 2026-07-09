4f9fecf454e0dd5485bf7c86b480ac0f95f9244b
CyberEloise <eloise.cyber.it@gmail.com>
2026-07-09 21:15:31 +0100

    Added cardlist to javascript, added images for each card

---
+++ assets/js/javascript.js
@@ -0,0 +1,17 @@
+var errors = 0;
+var cardlist  = [
+    "ahri",
+    "darius",
+    "ezreal",
+    "garen",
+    "katarina",
+    "leesin",
+    "leona",
+    "lux",
+    "missfortune",
+    "seraphine",
+    "thresh",
+    "vi",
+    "yasuo",
+    "zed",
+]

e205628ad32f3a26821e41800f8370b9aa1218a1
CyberEloise <eloise.cyber.it@gmail.com>
2026-07-10 01:23:29 +0100

    attatched cards and created flipped back const

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,17 +1,21 @@
-var errors = 0;
-var cardlist  = [
-    "ahri",
-    "darius",
-    "ezreal",
-    "garen",
-    "katarina",
-    "leesin",
-    "leona",
-    "lux",
-    "missfortune",
-    "seraphine",
-    "thresh",
-    "vi",
-    "yasuo",
-    "zed",
-]
+
+
+/** My cards with IDS names and an image path */
+const CARDS = [
+    { id: 1, name: "ahri", image: "assets/images/ahri.png" },
+    { id: 2, name: "darius", image: "assets/images/darius.png"},
+    { id: 3, name: "ezreal", iamge: "assets/images/ezreal.png"},
+    { id: 4, name: "garen", image: "assets/images/garen.png"},
+    { id: 5, name: "katarina", image: "assets/images/katarina.png"},
+    { id: 6, name: "lee_sin", image: "assets/images/lee_sin.png"},
+    { id: 7, name: "leona", image: "assets/images/leona.png"},
+    { id: 8, name: "lux", image: "assets/images/lux.png"},
+    { id: 9, name: "miss_fortune", image: "assets/images/miss_fortune.png"},
+    { id: 10, name: "seraphine", image: "assets/images/serpahine.png"},
+    { id: 11, name: "thresh", image: "assets/images/thresh.png"},
+    { id: 12, name: "vi", image: "assets/images/vi.png"},
+    { id: 13, name: "yasuo", image: "assets/images/yasuo.png"},
+    { id: 14, name: "zed", image: "assets/images/zed.png"},
+];
+
+const FLIPPED_CARD = "assets/images/back_of_card.png";
